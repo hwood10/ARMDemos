5c848df1e1b7530e92a81673d3529e3c341674d1
Greg Johnson <gregj@terrace.com>
2015-12-04 22:13:02 -0800

    Silent Install Oracle JRE

--- ARMDemos/VM Install Scripts/PlayServerInstall.sh
+++ ARMDemos/VM Install Scripts/PlayServerInstall.sh
@@ -3,6 +3,12 @@ sudo apt-get update
 sudo apt-get install python-software-properties
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
+
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+sudo debconf-set-selections
+
 sudo apt-get install -y oracle-java8-installer
 
 sudo apt-get install -y unzip
